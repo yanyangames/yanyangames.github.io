538db959481dd99b981e74baba1133c4e72d6f32
tommylee1992 <tommylee1992@gmail.com>
2016-08-09 01:23:04 -0400

    yanyan jie zhen zi su ver 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+var game;
+
+game = new Phaser.Game(600,600,Phaser.AUTO,'');
+
+game.state.add('Menu',Menu);
+
+game.state.add('Game',Game);
+
+game.state.add('Game_Over',Game_Over);
+
+
+
+game.state.start('Menu');
+
+// game.state.start('Game');
+
